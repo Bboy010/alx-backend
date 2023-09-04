6d03f53ca94090b6374e3d93226f17104e5c6abc
Bboy010 <hkoffianderson@gmail.com>
2023-09-03 18:15:53 -0700

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
+
+""" Task 3. Parameterize templates """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config(object):
-    """Config class"""
+class Config:
+    """Available languages configuration class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
-@babel.locale_selector
-def get_locale():
-    """get_locale function"""
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """index method"""
-    home_title = _("Welcome to Holberton")
-    home_header = _("Hello world!")
-    return render_template('3-index.html', home_title=home_title, home_header=home_header)
+    """returns a template"""
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
